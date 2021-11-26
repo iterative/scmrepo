f5190df5bcefd9dccdbf2d8e2a6f7ca105acfd73
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2021-11-26 13:29:17 +0545

    add dependency on pytest-test-utils

--- tests/test_scmrepo.py
+++ tests/test_scmrepo.py
@@ -1,27 +1,14 @@
-import os
 from unittest.mock import MagicMock
 
 from scmrepo.git import Git
 from scmrepo.progress import GitProgressEvent
 
 
-class ANY:
-    def __init__(self, expected_type):
-        self.expected_type = expected_type
-
-    def __repr__(self):
-        return "Any" + self.expected_type.__name__.capitalize()
-
-    def __eq__(self, other):
-        return isinstance(other, self.expected_type)
-
-
-def test_clone(tmp_path: os.PathLike):
-    os.chdir(tmp_path)
+def test_clone(tmp_dir: TmpDir):
     progress = MagicMock()
     url = "https://github.com/iterative/dvcyaml-schema"
 
     Git.clone(url, "dir", progress=progress)
 
     progress.assert_called_with(ANY(GitProgressEvent))
-    assert (tmp_path / "dir").exists()
+    assert (tmp_dir / "dir").exists()
