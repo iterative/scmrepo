de8d16a2ef44ce8784c8d8021c5613babb17e451
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2021-11-30 19:47:28 +0545

    tests: add email and user

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,11 @@ from scmrepo.git import Git
 
 
 @pytest.fixture
-def scm(tmp_dir: TmpDir):
+def scm(tmp_dir: TmpDir, monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setenv("GIT_CONFIG_NOSYSTEM", "1")
+    monkeypatch.setenv("GIT_AUTHOR_NAME", "DVC test user")
+    monkeypatch.setenv("GIT_AUTHOR_EMAIL", "dvctester@example.com")
+
     git_ = Git.init(tmp_dir)
     yield git_
     git_.close()
