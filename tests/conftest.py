a76b7193d97dae234d8d1692d6072cdeb7bfa647
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2021-11-30 20:17:43 +0545

    isolate

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,46 @@
+import os
+import sys
+
+import pygit2
 import pytest
-from pytest_test_utils import TmpDir
+from pytest_test_utils import TempDirFactory, TmpDir
 
 from scmrepo.git import Git
 
 
-@pytest.fixture
-def scm(tmp_dir: TmpDir, monkeypatch: pytest.MonkeyPatch):
-    monkeypatch.setenv("GIT_CONFIG_NOSYSTEM", "1")
-    monkeypatch.setenv("GIT_AUTHOR_NAME", "DVC test user")
-    monkeypatch.setenv("GIT_AUTHOR_EMAIL", "dvctester@example.com")
+@pytest.fixture(autouse=True)
+def isolate(tmp_dir_factory: TempDirFactory, monkeypatch: pytest.MonkeyPatch):
+    path = tmp_dir_factory.mktemp("mock")
+    home_dir = path / "home"
+    home_dir.mkdir()
 
+    if sys.platform == "win32":
+        home_drive, home_path = os.path.splitdrive(home_dir)
+        monkeypatch.setenv("USERPROFILE", str(home_dir))
+        monkeypatch.setenv("HOMEDRIVE", home_drive)
+        monkeypatch.setenv("HOMEPATH", home_path)
+    else:
+        monkeypatch.setenv("HOME", str(home_dir))
+
+    monkeypatch.setenv("GIT_CONFIG_NOSYSTEM", "1")
+    contents = b"""
+[user]
+name=DVC Tester
+email=dvctester@example.com
+[init]
+defaultBranch=master
+"""
+    (home_dir / ".gitconfig").write_bytes(contents)
+    pygit2.settings.search_path[pygit2.GIT_CONFIG_LEVEL_GLOBAL] = str(home_dir)
+
+
+@pytest.fixture
+def scm(tmp_dir: TmpDir):
     git_ = Git.init(tmp_dir)
+    sig = git_.pygit2.default_signature
+
+    assert sig.email == "dvctester@example.com"
+    assert sig.name == "DVC Tester"
+
     yield git_
     git_.close()
