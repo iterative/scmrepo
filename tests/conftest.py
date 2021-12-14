cb97b8cff8eb7f2234942a9a506adb5a1ee5d51c
Saugat Pachhai <suagatchhetri@outlook.com>
2021-12-14 18:19:51 +0545

    tests: remove unnecessary check for docker (#19)
    * tests: remove unnecessary check for docker
    
    * skip docker tests on Windows + GHA

--- tests/conftest.py
+++ tests/conftest.py
@@ -78,16 +78,16 @@ def scm(tmp_dir: TmpDir) -> Iterator[Git]:
 def docker(request: pytest.FixtureRequest):
     for cmd in [("docker", "ps"), ("docker-compose", "version")]:
         try:
-            subprocess.call(
-                " ".join(cmd),
+            subprocess.check_call(
+                cmd,
                 stderr=subprocess.DEVNULL,
                 stdout=subprocess.DEVNULL,
             )
         except (subprocess.CalledProcessError, OSError):
             pytest.skip(f"no {cmd[0]} installed")
 
-    if "CI" in os.environ and sys.platform != "linux":
-        pytest.skip(f"skip for {sys.platform} in CI")
+    if "CI" in os.environ and os.name == "nt":
+        pytest.skip("disabled for Windows on Github Actions")
 
     pytest.importorskip("pytest_docker")
     yield request.getfixturevalue("docker_services")
