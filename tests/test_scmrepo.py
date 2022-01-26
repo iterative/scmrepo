55e42c2fec3fa8501161eb9ab5d3c8d540ec3559
Peter Rowlands <peter@pmrowla.com>
2022-01-26 16:30:28 +0900

    tests: add test for clone(shallow_branch=...)

--- tests/test_scmrepo.py
+++ tests/test_scmrepo.py
@@ -18,3 +18,13 @@ def test_clone(tmp_dir: TmpDir, matcher: Type[Matcher]):
 
     progress.assert_called_with(matcher.instance_of(GitProgressEvent))
     assert (tmp_dir / "dir").exists()
+
+
+def test_clone_shallow(tmp_dir: TmpDir):
+    url = "https://github.com/iterative/dvcyaml-schema"
+    shallow_branch = "master"
+
+    repo = Git.clone(url, "dir", shallow_branch=shallow_branch)
+    shallow_file = tmp_dir / "dir" / ".git" / "shallow"
+    assert shallow_file.exists()
+    assert repo.get_rev() in shallow_file.read_text().splitlines()
