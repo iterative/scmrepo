4256032d6b1cab348e7e83a9d6084c84a6e373e2
Ivan Shcheklein <shcheklein@gmail.com>
2023-08-20 11:45:17 -0700

    feat(pygit2): signature doen't break if env is set and git conf not

--- tests/test_pygit2.py
+++ tests/test_pygit2.py
@@ -1,9 +1,12 @@
 # pylint: disable=unused-argument
+import os
+
 import pygit2
 import pytest
 from pytest_mock import MockerFixture
 from pytest_test_utils import TmpDir
 
+from scmrepo.exceptions import SCMError
 from scmrepo.git import Git
 from scmrepo.git.backend.pygit2 import Pygit2Backend
 
@@ -68,8 +71,41 @@ def test_pygit_stash_apply_conflicts(
         "ssh://login@server.com:12345/repository.git",
     ],
 )
-def test_pygit2_ssh_error(tmp_dir: TmpDir, scm: Git, url):
+def test_pygit_ssh_error(tmp_dir: TmpDir, scm: Git, url):
     backend = Pygit2Backend(tmp_dir)
     with pytest.raises(NotImplementedError):
         with backend.get_remote(url):
             pass
+
+
+@pytest.mark.parametrize("name", ["committer", "author"])
+def test_pygit_use_env_vars_for_signature(
+    tmp_dir: TmpDir, mocker: MockerFixture, name: str
+):
+    from pygit2 import Signature
+
+    mocker.patch(
+        "scmrepo.git.Pygit2Backend.default_signature",
+        new=mocker.PropertyMock(side_effect=SCMError),
+    )
+    git = Git.init(tmp_dir)
+    with pytest.raises(SCMError):
+        git.pygit2.default_signature  # pylint: disable=W0104
+
+    # Make sure that the environment variables are not set to not interfere with
+    # with the check below
+    for var in [f"GIT_{name.upper()}_NAME", f"GIT_{name.upper()}_EMAIL"]:
+        assert os.environ.get(var, None) is None
+
+    # Basic expected behavior if vars are not set. Another sanity check
+    with pytest.raises(SCMError):
+        getattr(git.pygit2, name)
+
+    mocker.patch.dict(os.environ, {f"GIT_{name.upper()}_EMAIL": "olivaw@iterative.ai"})
+    with pytest.raises(SCMError):
+        getattr(git.pygit2, name)
+
+    mocker.patch.dict(os.environ, {f"GIT_{name.upper()}_NAME": "R. Daneel Olivaw"})
+    assert getattr(git.pygit2, name) == Signature(
+        email="olivaw@iterative.ai", name="R. Daneel Olivaw"
+    )
