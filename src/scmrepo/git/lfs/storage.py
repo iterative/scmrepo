6861a498fade9abf7c90dd6a569c60aaace59a22
Peter Rowlands <peter@pmrowla.com>
2023-11-07 15:56:01 +0900

    lfs: support configured lfs.storage

--- src/scmrepo/git/lfs/storage.py
+++ src/scmrepo/git/lfs/storage.py
@@ -1,10 +1,13 @@
 import errno
 import os
-from typing import BinaryIO, Collection, Optional, Union
+from typing import TYPE_CHECKING, BinaryIO, Collection, Optional, Union
 
 from dvc_objects.fs.callbacks import TqdmCallback
 
 from .pointer import Pointer
+
+if TYPE_CHECKING:
+    from scmrepo.git import Git
 
 
 class LFSStorage:
@@ -50,3 +53,17 @@ class LFSStorage:
 
     def close(self):
         pass
+
+
+def get_storage_path(scm: "Git") -> str:
+    """Return the LFS storage directory for the specified repository."""
+
+    config = scm.get_config()
+    git_dir = scm._get_git_dir(scm.root_dir)  # pylint: disable=protected-access
+    try:
+        path = config.get(("lfs",), "storage")
+        if os.path.isabs(path):
+            return path
+    except KeyError:
+        path = "lfs"
+    return os.path.join(git_dir, path)
