79a32210eb9891a910678852f3a92f87c880ce25
Helio Machado <0x2b3bfa0+git@googlemail.com>
2024-01-28 16:09:07 +0000

    Drop Python 3.8

--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ nox.options.sessions = "lint", "tests"
 locations = "src", "tests"
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
+@nox.session(python=["3.9", "3.10", "3.11"])
 def tests(session: nox.Session) -> None:
     session.install(".[tests]")
     session.run(
