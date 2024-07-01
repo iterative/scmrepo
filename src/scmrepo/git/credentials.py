fe199f3bc0989f1ad8dc52a3912fab3088be50ea
Ivan Shcheklein <shcheklein@gmail.com>
2024-07-01 15:58:57 -0700

    revert noqa changed

--- src/scmrepo/git/credentials.py
+++ src/scmrepo/git/credentials.py
@@ -136,7 +136,7 @@ class GitCredentialHelper(CredentialHelper):
         if not shutil.which(executable) and shutil.which("git"):
             # If the helper cannot be found in PATH, it might be
             # a C git helper in GIT_EXEC_PATH
-            git_exec_path = subprocess.check_output(
+            git_exec_path = subprocess.check_output(  # noqa: S603
                 ("git", "--exec-path"),
                 text=True,
             ).strip()
@@ -158,7 +158,7 @@ class GitCredentialHelper(CredentialHelper):
         helper_input.append("")
 
         try:
-            res = subprocess.run(
+            res = subprocess.run(  # noqa: S603
                 cmd,
                 check=True,
                 capture_output=True,
@@ -199,7 +199,7 @@ class GitCredentialHelper(CredentialHelper):
         helper_input.append("")
 
         try:
-            res = subprocess.run(
+            res = subprocess.run(  # noqa: S603
                 cmd,
                 capture_output=True,
                 input="\n".join(helper_input),
@@ -224,7 +224,7 @@ class GitCredentialHelper(CredentialHelper):
         helper_input.append("")
 
         try:
-            res = subprocess.run(
+            res = subprocess.run(  # noqa: S603
                 cmd,
                 capture_output=True,
                 input="\n".join(helper_input),
@@ -448,7 +448,7 @@ class _AskpassCommand:
     def input(self, prompt: str) -> str:
         argv = [self.command, prompt]
         try:
-            res = subprocess.run(
+            res = subprocess.run(  # noqa: S603
                 argv,
                 check=True,
                 capture_output=True,
